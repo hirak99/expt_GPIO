70c76039e796155b89c615c4fbb9df04cdb19acc
Arnab <hirak99@gmail.com>
2019-05-12 22:47:42 -0700

    Coded morse code encoder.

--- gpio_led.py
+++ gpio_led.py
@@ -2,17 +2,16 @@ import RPi.GPIO as GPIO
 import time
 
 LED_PIN = 17
-SEQUENCE = "10100000"
+SEQUENCE = "10100000" * 3
 BEAT = 0.125
 
 def code():
   GPIO.setmode(GPIO.BCM)
   GPIO.setup(LED_PIN, GPIO.OUT)
-  for _ in range(5):
-    for state in SEQUENCE:
-      to_set = GPIO.HIGH if state == "0" else GPIO.LOW
-      GPIO.output(LED_PIN, int(to_set))
-      time.sleep(BEAT)
+  for state in SEQUENCE:
+    to_set = GPIO.HIGH if state == "0" else GPIO.LOW
+    GPIO.output(LED_PIN, int(to_set))
+    time.sleep(BEAT)
 
 def main():
   print("Running...")
---
+++ morse/morse.py
@@ -0,0 +1,70 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+
+LED_PIN = 17
+BEAT = 0.125
+
+class Morse(object):
+  def __init__(self):
+    self._lookup = self._get_codes()
+
+  @staticmethod
+  def _get_codes():
+    # http://ascii-table.com/morse-code.php
+    # Space between characters is already 3 units.
+    # Space between words (' ') is two more units (total 5 units).
+    lookup = {' ': '00'}
+    for line in open('morsecode_lookup.txt'):
+      line = line.strip().replace(' ', '')
+      if not line:
+        # Skip empty lines.
+        continue
+      code = line[1:].replace('.', '10').replace('-', '1110')
+      lookup[line[0]] = code
+    return lookup
+
+  def _letter_to_morse(self, letter):
+    return self._lookup[letter.upper()]
+
+  def to_sequence(self, letters):
+    code = []
+    for i, letter in enumerate(letters):
+      if i != 0:
+        code.append(('', '000'))
+      code.append((letter, self._letter_to_morse(letter)))
+    return code
+
+def code(letters):
+  morse_encoder = Morse()
+  encoded = morse_encoder.to_sequence(letters)
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(LED_PIN, GPIO.OUT)
+  for letter, sequence in encoded:
+    print(letter, end='', flush=True)
+    for state in sequence:
+      if state == "0":
+        GPIO.output(LED_PIN, GPIO.LOW)
+      elif state == "1":
+        GPIO.output(LED_PIN, GPIO.HIGH)
+      else:
+        raise RuntimeError("Unknown character in seq.")
+      time.sleep(BEAT)
+  print()
+
+def main():
+  print("Running...")
+  if len(sys.argv) > 1:
+    to_encode = ' '.join(sys.argv[1:])
+  else:
+    to_encode = 'Hello world.'
+  try:
+    code(to_encode)
+  finally:
+    print("Cleaning up...")
+    GPIO.cleanup()
+  print("Done.")
+
+if __name__ == "__main__":
+  main()
+
